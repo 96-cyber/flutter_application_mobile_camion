a9cda84d30e640f277f29b98b7904ddef19f7111
96-cyber <khounimohamedali019@gmail.com>
2025-03-25 19:52:36 +0100

    camion projet

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter_application_mobile_camion/loginScreens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+   Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+    );
+  }
+}
